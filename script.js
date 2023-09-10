51467e62688157b3706733da5955693f7904a0ac
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-09-10 15:15:43 +0530

    Remove cell numbers and add basic css for cells

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ const mainContainer = document.querySelector('.main-container');
 
 const gridCell = document.createElement('div');
 gridCell.classList.add('cell')
-gridCell.innerText = 'Hello'
 
 const gridRow = document.createElement('div');
 gridRow.classList.add('row')
 
 
 for(let i = 1; i<=16; i++){
-    gridCell.innerText = i;
     gridRow.appendChild(gridCell.cloneNode(true));
 }
 for(let i = 1; i<=16; i++){
