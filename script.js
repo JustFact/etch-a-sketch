ee4d4f95ebe5998d0a3bffe17af3013d03a922b1
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-09-10 15:08:12 +0530

    Add 16x16 divs of Rows x Cells

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const mainContainer = document.querySelector('.main-container');
+
 const gridCell = document.createElement('div');
+gridCell.classList.add('cell')
+gridCell.innerText = 'Hello'
+
+const gridRow = document.createElement('div');
+gridRow.classList.add('row')
+
+
+for(let i = 1; i<=16; i++){
+    gridCell.innerText = i;
+    gridRow.appendChild(gridCell.cloneNode(true));
+}
+for(let i = 1; i<=16; i++){
+    mainContainer.appendChild(gridRow.cloneNode(true));
+}
+
+
+console.log(mainContainer)
