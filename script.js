a61c76e56cb8f3f49298e1c3c163d9d528ec1ac3
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-09-10 21:05:23 +0530

    Add logic for dynamic dimensions and cell sizing

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const canvasDimensions = 600;
+const numberOfCells = 20;
+
 const mainContainer = document.querySelector('.main-container');
 mainContainer.addEventListener('mouseover', (event)=>{
     if(event.target.classList.contains('cell')){
@@ -7,14 +10,19 @@ mainContainer.addEventListener('mouseover', (event)=>{
 
 const gridCell = document.createElement('div');
 gridCell.classList.add('cell')
+function setCellSize(cellSize){
+    gridCell.style.width = `${cellSize}px`
+    gridCell.style.height = `${cellSize}px`
+}
+setCellSize(canvasDimensions/numberOfCells);
 
 const gridRow = document.createElement('div');
 gridRow.classList.add('row')
 
 
-for(let i = 1; i<=16; i++){
+for(let i = 1; i<=numberOfCells; i++){
     gridRow.appendChild(gridCell.cloneNode(true));
 }
-for(let i = 1; i<=16; i++){
+for(let i = 1; i<=numberOfCells; i++){
     mainContainer.appendChild(gridRow.cloneNode(true));
 }
