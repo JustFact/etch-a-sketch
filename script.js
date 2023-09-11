46a4848b22e71c081695e47e00c7869eea2ebf0c
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-09-11 21:59:09 +0530

    Update grid with new cell count

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 const canvasDimensions = 600;
-let numberOfCells = 20;
+let numberOfCells = 16;
 
-const mainContainer = document.querySelector('.main-container');
+let mainContainer = document.querySelector('.main-container');
 mainContainer.addEventListener('mouseover', (event)=>{
     if(event.target.classList.contains('cell')){
         event.target.style.backgroundColor = 'purple'
     }
 })
 
-const gridCell = document.createElement('div');
+let gridCell = document.createElement('div');
 gridCell.classList.add('cell')
 function setCellSize(cellSize){
     gridCell.style.width = `${cellSize}px`
     gridCell.style.height = `${cellSize}px`
 }
 setCellSize(canvasDimensions/numberOfCells);
-
-const gridRow = document.createElement('div');
-gridRow.classList.add('row')
 
 const nCells = document.querySelector('.set-ncell-btn');
 nCells.addEventListener('click',setNumberOfCells)
@@ -31,11 +28,34 @@ function setNumberOfCells(){
     }else{
         return;
     }
+    updateGrid()
 }
 
-for(let i = 1; i<=numberOfCells; i++){
-    gridRow.appendChild(gridCell.cloneNode(true));
+function createGrid(){
+    let gridRow = document.createElement('div');
+    gridRow.classList.add('row')
+    
+    for(let i = 1; i<=numberOfCells; i++){
+        gridRow.appendChild(gridCell.cloneNode(true));
+    }
+    for(let i = 1; i<=numberOfCells; i++){
+        mainContainer.appendChild(gridRow.cloneNode(true));
+    }
 }
-for(let i = 1; i<=numberOfCells; i++){
-    mainContainer.appendChild(gridRow.cloneNode(true));
+
+function cleanGrid(){
+    while(mainContainer.firstChild){
+        while(mainContainer.firstChild.firstChild){
+            mainContainer.firstChild.removeChild(mainContainer.firstChild.firstChild)
+        }
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
 }
+
+function updateGrid(){
+    cleanGrid();
+    setCellSize(canvasDimensions/numberOfCells);
+    createGrid();
+}
+
+createGrid();
